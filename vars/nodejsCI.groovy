2f6e10cb39a488cf3dc1a9ade071d01e466bd71f
leonard-waters <leonard.waters@outlook.com>
2018-06-18 09:03:32 -0700

    Add NodeJs Template and NodeJsCI

--- vars/nodejsCI.groovy
+++ vars/nodejsCI.groovy
@@ -2,17 +2,42 @@
 def call(Map config = [:], List tags = [], String acct = '', body) {
   stage('install dependencies') {
     container('nodejs-build') {
-      sh "yarn install; yarn run build"
-      sh "yarn install --production"
+      sh 'yarn version --no-git-tag-version'
+      sh 'yarn install;'
+    }
+  }
+
+  stage('lint') {
+    try {
+      sh 'yarn lint -f junit -o lint-report.xml'
+    } finally {
+      junit 'lint-report.xml'
     }
   }
 
   stage('test') {
-
+    try {
+      sh 'yarn add jest jest-junit --dev'
+      withEnv(["JEST_JUNIT_OUTPUT=./jest-test-results.xml"]) {
+				sh 'yarn test --ci --testResultsProcessor="jest-junit"'
+			}
+    } finally {
+      junit 'unit-test-report.xml'
+    }
   }
 
   stage('build release') {
+    sh 'webpack --config webpack.prod.js --bail --display-error-details'
+  }
+
+  stage('validate helm chart') {
+    def version_tag = tags.get(0)
     
+    helmValidate(config, version_tag)
+  }
+
+  stage('docker build and publish') {
+    dockerBuildAndPublish(config, tags, acct)
   }
 
   body()
---
+++ vars/nodejsRelease.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/groovy
+def call(Map config, List tags, String acct) {
+  //TODO:
+  //Implement Service Now portion
+  //If test deployment do not run service now
+
+  def version_tag = tags.get(0)
+  
+  nodejsCI(config, tags, acct){}
+
+  standardRelease(config, version_tag){}
+}
