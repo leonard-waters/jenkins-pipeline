ef54bee44e5af754eefeab8e95424de19e7066ea
leonard-waters <leonard.waters@outlook.com>
2018-06-19 14:23:41 -0700

    Remove ElixirCI from Release

--- vars/elixirRelease.groovy
+++ vars/elixirRelease.groovy
@@ -5,8 +5,6 @@ def call(Map config, List tags, String acct) {
   //If test deployment do not run service now
 
   def version_tag = tags.get(0)
-  
-  elixirCI(config, tags, acct){}
 
   standardRelease(config, version_tag){}
 }
